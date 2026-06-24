50d6708395197a63b9dda9e97c633740abf3fe6a
Talles Gomes <tallesthrasht666@gmail.com>
2026-06-24 17:55:34 -0300

    feat: configure Flyway and add item migration

--- dota-analytics/sql/v002__create_item.sql
+++ dota-analytics/sql/v002__create_item.sql
@@ -1,11 +1,9 @@
--- Cria tabela de items
-
 CREATE TABLE item (
     
    id               INTEGER PRIMARY KEY,
    name             VARCHAR (255) NOT NULL,
-   dname            VARCHAR (255) NOT NULL,
-   qual             VARCHAR (255),
+   dname            VARCHAR (255),
+   qual             VARCHAR (50),
    cost             INTEGER,
    behavior         VARCHAR (100),
    mc               BOOLEAN,
